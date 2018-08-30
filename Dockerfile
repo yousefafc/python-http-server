0260cd64e5b4a2f922aa0f1ebdafe25c3cd34300
bob <bob.crutchley.qa@gmail.com>
2018-08-30 11:49:03 +0100

    updated app.py and Dockerfile so that the current working directory is handled in the application only, not by docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM python
 COPY app.py /opt/python-http-server/app.py
 COPY public /opt/python-http-server/public
-WORKDIR /opt/python-http-server/public
 ENTRYPOINT ["/opt/python-http-server/app.py"]
 
--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 import SimpleHTTPServer
 import SocketServer
+import os
 
 PORT = 9000
-
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
 httpd = SocketServer.TCPServer(("", PORT), Handler)
-
 print "serving at port", PORT
+os.chdir(os.path.dirname(os.path.realpath(__file__)) + "/public")
 httpd.serve_forever()
+
