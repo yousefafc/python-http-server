920e055a5a0fe823cfbf62a23cc6aa62a4d519ba
bob <bob.crutchley.qa@gmail.com>
2018-08-31 09:55:28 +0100

    added gitignore, index page includes a version and hostname

--- app.py
+++ app.py
@@ -2,11 +2,22 @@
 import SimpleHTTPServer
 import SocketServer
 import os
+import socket
+import json
 
+# create info.json
+os.chdir(os.path.dirname(os.path.realpath(__file__)) + "/public")
+info = {
+	"host_name": socket.gethostname(),
+	"version": 1
+}
+with open('info.json', 'w') as outfile:
+    json.dump(info, outfile)
+
+# configure and run the server
 PORT = 9000
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
-print "serving at port", PORT
-os.chdir(os.path.dirname(os.path.realpath(__file__)) + "/public")
+print socket.gethostname()
 httpd.serve_forever()
 
